b72c705ac7d3f16e497b4f704e1262ac65befd3f
Belu-Mom <111750144+Belu-Mom@users.noreply.github.com>
2023-03-07 17:35:06 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,102 @@
-var express = require('express');
-var router = express.Router();
-var user=require('./users');
-var passport=require('passport');
-const localStratergy=require("passport-local");
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const userModel = require("./users");
+const postModel = require('./post')
+const { body, validationResult } = require('express-validator');
 
+const data = new userModel()
 
-router.post("/reg",function(req,res){
-  const dets= new userModal({
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()));
+
+/* GET home page. */
+router.get("/", function (req, res) {
+  res.render("index",{errors:false});
+});
+
+router.post("/reg", body('password').isLength({ min: 5 }).withMessage("Password should be of minimum 5 characters"),
+function (req, res) {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // return res.status(400).json({ errors: errors.array() });
+    res.render('index',errors)
+  }
+
+  const dets = new userModel({
     name: req.body.name,
     username: req.body.username,
     email: req.body.email,
   });
-userModel.register(dets,req.body.password).then(function(registeredUser){
-  passport.authenticate("local")(req,res,function(){
-    res.redirect("/profile");
-  });
-});
-})
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('registration');
-});
-router.get('/login', function(req, res) {
-  res.render('login');
-});
-router.get('/profile', function(req, res) {
-  res.render('profile');
-});
-router.get("/profile",isLoggedIn,function(req,res){
-  userModel.findOne({username:req.session.passport.user})
-  .then(function(user){
-    res.render('profile',{user});
+
+  userModel.register(dets, req.body.password).then(function (registeredUser) {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
   });
 });
 
-function isLoggedIn(req,res,next){
-  if (req.isAuthenticated()){
+router.get("/profile",isLoggedIn ,function (req, res) {
+  userModel.findOne({username:req.session.passport.user})
+  .then(function(user){
+    res.render('profile',{user});
+  })
+});
+
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logOut();
+  res.redirect("/");
+});
+
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
     return next();
   } else {
     res.redirect("/login");
   }
 }
-router.post(
-  "/login",
-  passport.authenticate("local",{
-    successRedirect: "/profile",
-    failureRedirect: "/login",
-  }),
-  function(req,res){}
-)
-router.get("/logout",function(req,res){
-  req.logOut();
-  res.redirect("/");
+
+router.get("/timeline", function (req, res) {
+  userModel.findOne({username:req.session.passport.user}).populate('posts')
+  .then(function(foundUser){
+    res.render("timeline",{foundUser});
+  })
 });
+
+
+router.post('/post', function (req, res) {
+  userModel.findOne({ username: req.session.passport.user })
+    .then(function (foundUser) {
+      postModel.create({
+        postText: req.body.post,
+        user: foundUser
+      }).then(function (newlyCreatedPost) {
+        foundUser.posts.push(newlyCreatedPost);
+        foundUser.save().then(function () {
+          res.redirect('/timeline');
+        })
+      })
+    })
+});
+
+
+
 module.exports = router;
